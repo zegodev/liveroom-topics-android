9a8db381c1eb3f9b859ae3ac67f0f656e7bb3b95
hsx2117 <32176104+hsx2117@users.noreply.github.com>
2019-07-04 22:12:41 +0800

    Update ZGManager.java

--- src/LiveRoomPlayground/common/src/main/java/com/zego/common/ZGManager.java
+++ src/LiveRoomPlayground/common/src/main/java/com/zego/common/ZGManager.java
@@ -16,24 +16,9 @@ public class ZGManager {
 
     private boolean isTestEnv = false;
 
-    /**
-     * 请提前在即构管理控制台获取 appID 与 appSign
-     * AppID 填写样式示例：
-     *      public static long appId = 123456789L ;
-     * appSign 填写样式示例：
-     *      public static final byte[] appSign = {
-     *          (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-     *          (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-     *          (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-     *          (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-     *          (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-     *          (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-     *          (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-     *          (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00
-     *      };
-     **/
-    public static byte[] appSign = ;
-    public static long appId = ;
+
+    public static byte[] appSign = GetAppIdConfig.appSign;
+    public static long appId = GetAppIdConfig.appId;
 
     public ZegoLiveRoom api() {
         // 判断是否创建了ZegoLiveRoom实例 避免重复初始化sdk
